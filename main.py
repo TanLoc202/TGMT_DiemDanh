c9905e451ee7af9163cb1129d8f146fa48bbcae2
TanLoc202 <Locshadow202@gmail.com>
2024-05-09 14:03:26 +0700

    commit 2

---
+++ main.py
@@ -0,0 +1,23 @@
+import cv2
+import os
+
+def chupanh(name, code):
+    if not os.path.exists("image"):
+        os.mkdir("image")
+
+    camera = cv2.VideoCapture(0)
+    stt = 1
+    while camera.isOpened() and stt <21:
+        ret, frame = camera.read()
+        if not ret:
+            print("Không thể đọc frame từ webcam.")
+            break
+        else:
+            file_path = f'images/{name}.{code}.{stt}.jpg'
+            cv2.imwrite(file_path, frame)
+            print(file_path)
+            stt += 1
+        cv2.waitKey(1)
+
+if __name__=="__main__":
+    chupanh("Loc", "110120142")
