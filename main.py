b0d890b7706768d0bd02c1237cd5f43c85ea51ca
TanLoc202 <Locshadow202@gmail.com>
2024-05-21 09:50:45 +0700

    update main

--- main.py
+++ main.py
@@ -10,11 +10,14 @@ if __name__=="__main__":
     os.system("cls")
     print("1. Chạy chương trình nhận diện")
     print("2. Chạy chương trình nhập thông tin")
-    print("3. Chạy chương trình huấn luyện")
+    print("3. Chạy chương trình nhập khuôn mặt")
+    print("4. Chạy chương trình huấn luyện")
     key = int(input("Run>>"))
     if key == 1: 
         nhan_dien.run()
     elif key == 2:
         nhap_lieu.run()
     elif key == 3:
+        nhan_dien.nhap_khuon_mat()
+    elif key == 4:
         nhan_dien.huan_luyen_model()
